0dc2dfe5bfcc9b5e2c6a2399e437c250dee7d9ed
Critteros <szymonkoziol3@gmail.com>
2021-02-28 23:17:36 +0100

    Werid bug

---
+++ src/commands/first/meow.ts
@@ -0,0 +1,17 @@
+import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+
+export default class MeowCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'meow',
+      aliases: ['kitty-cat'],
+      group: 'first',
+      memberName: 'meow',
+      description: 'Replies with a meow, kitty cat.',
+    });
+  }
+
+  run(message: CommandoMessage) {
+    return message.say('Meow!');
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { CommandoClient } from 'discord.js-commando';
+import { CommandoClient, Command } from 'discord.js-commando';
 import { config } from 'dotenv';
+import * as path from 'path';
 
 config();
 
@@ -7,7 +8,15 @@ const client = new CommandoClient({
   commandPrefix: process.env.COMMAND_PREFIX,
 });
 
-client.on('ready', () => console.log('hi'));
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([['first', 'testing group']])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+console.log(path.join(__dirname, 'commands'));
+client.on('ready', () => console.log('ready'));
 
 client.login(process.env.CLIENT_TOKEN);
 
