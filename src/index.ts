17148a32661637e29674611da70531f039ad2099
Critteros <szymonkoz@student.agh.edu.pl>
2021-03-01 19:33:29 +0100

    The //  Sword  ⚔️

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ const client = new CommandoClient({
 });
 
 client.on('ready', () => console.log('hi'));
-client.on('message', () => console.log('Message recived'))
+//client.on('message', () => console.log('Message recived'))
 
 client.login(process.env['CLIENT_TOKEN']);
