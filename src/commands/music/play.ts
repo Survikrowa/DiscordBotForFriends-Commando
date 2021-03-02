257d6fb8dee02e5fb335e089c608b11e639c765e
survikrowa <szymonhrabia60@gmail.com>
2021-03-02 10:04:35 +0100

    add inital version of music bot

---
+++ src/commands/music/play.ts
@@ -0,0 +1,31 @@
+import { Command } from 'discord.js-commando';
+import { distube } from '../../index';
+import type { CommandoClient, CommandoMessage } from 'discord.js-commando';
+import { Message } from 'discord.js';
+
+type YoutubeArguments = {
+  musicTitle: string;
+};
+
+export default class PlayMusic extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'play',
+      group: 'music',
+      memberName: 'playmusic',
+      description: 'join channel and play music',
+      args: [
+        {
+          key: 'musicTitle',
+          prompt: 'What music would you like play?',
+          type: 'string',
+        },
+      ],
+    });
+  }
+  run(msg: CommandoMessage, { musicTitle }: YoutubeArguments): Promise<Message | Message[] | null> | null {
+    console.log(musicTitle);
+    distube.play(msg, musicTitle);
+    return msg.channel.send(`Playing ${musicTitle}`);
+  }
+}
