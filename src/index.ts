f3c428401a0dc410b52c960454cbd5fb13890de8
Critteros <szymonkoziol3@gmail.com>
2021-03-06 01:25:27 +0100

    Added firebase initializer and changed targets to ES2017

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import { config } from 'dotenv';
 import Distube from 'distube';
 import * as path from 'path';
 
+//Firebase stuff
+import admin from 'firebase-admin';
+import firebase_token from '../firebase.json';
+
+//Loading env values
 config();
 
+//Init Commodoro client
 const client = new CommandoClient({
   commandPrefix: process.env.COMMAND_PREFIX,
 });
 
+//Registering commands
 client.registry
   .registerDefaultTypes()
   .registerGroups([
@@ -17,10 +24,17 @@ client.registry
   ])
   .registerCommandsIn({ filter: /^([^.].*)\.(js|ts)$/, dirname: path.join(__dirname, 'commands') });
 
+//Distube client
 export const distube = new Distube(client, {
   searchSongs: false,
   emitNewSongOnly: true,
   youtubeCookie: process.env.COOKIE,
+});
+
+//Firebase init
+admin.initializeApp({
+  credential: admin.credential.cert(firebase_token as admin.ServiceAccount),
+  storageBucket: 'gs://discordbot-44c37.appspot.com',
 });
 
 distube.on('error', (message, error) => {
