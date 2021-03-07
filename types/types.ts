b131c10e9ed7dc4d3d1b7a723b3a1f5a908e07cb
survikrowa <szymonhrabia60@gmail.com>
2021-03-07 14:40:37 +0100

    update types for COmmandsWithoutPrefix to handle async function with void as a return

--- types/types.ts
+++ types/types.ts
@@ -4,7 +4,7 @@ export type GetCommandWithoutPrefix = (message: Message) => CommandWithoutPrefix
 
 export type CommandWithoutPrefix = {
   title: string;
-  run: () => Promise<Message | MessageReaction> | void;
+  run: () => Promise<Message | MessageReaction> | void | Promise<void>;
 };
 
 export type CommandsWithoutPrefixes = (message: Message) => CommandWithoutPrefix[];
