eddbf8915a1c7a7d18c211aea80861e2e700fbec
survikrowa <szymonhrabia60@gmail.com>
2021-03-07 14:41:48 +0100

    Backport pog command and add storage support for it

--- src/commandsWithoutPrefixes/commands.ts
+++ src/commandsWithoutPrefixes/commands.ts
@@ -3,10 +3,12 @@ import { boopCommand } from './commands/boop';
 import { animeCommand } from './commands/anime';
 import { gTwoCommand } from './commands/g2';
 import { lolCommand } from './commands/lol';
+import { pogCommand } from './commands/pog';
 
 export const commandsWithoutPrefixes: CommandsWithoutPrefixes = (message) => [
   boopCommand(message),
   animeCommand(message),
   gTwoCommand(message),
   lolCommand(message),
+  pogCommand(message),
 ];
