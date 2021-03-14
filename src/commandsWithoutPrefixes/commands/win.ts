7d6973af9881f91f9f1a1502001a62513304d4af
Critteros <szymonkoziol3@gmail.com>
2021-03-14 01:18:13 +0100

    Win patch

--- src/commandsWithoutPrefixes/commands/win.ts
+++ src/commandsWithoutPrefixes/commands/win.ts
@@ -3,5 +3,17 @@ import { distube } from '../../index';
 
 export const winCommand: GetCommandWithoutPrefix = (message) => ({
   title: 'win',
-  run: () => distube.play(message, 'https://www.youtube.com/watch?v=a3npXG2hh_I'),
+  run: async () => {
+    //Constant used to retrive user info
+    const guildID = message.guild?.id;
+    const userID = message.author.id;
+    const client = message.author.client;
+
+    const guild = client.guilds.cache.get(guildID as string);
+    const user = guild?.members.cache.get(userID);
+
+    if (user?.voice.channel) {
+      distube.play(message, 'https://www.youtube.com/watch?v=a3npXG2hh_I');
+    }
+  },
 });
